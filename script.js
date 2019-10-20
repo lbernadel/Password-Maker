8d47ecc75a61d3488e677939592b396ed5aef63a
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2019-10-19 22:09:56 -0400

    tried getting computer to choose random characters and make a string to display password; stuck in initial if statement

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz",
   uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   numbers = "0123456789",
   specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
+  password = "",
   generateButton = document.getElementById("generate"),
   copyButton = document.getElementById("copy");
 
+// initiate generate function with button click
   generateButton.addEventListener("click", generate);
 
   function generate() {
@@ -16,9 +18,9 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz",
         numPicked = true,
         specialPicked = true;
 
-    // alert("You picked " + charNum);
+    // asking for length needed & validating it's within range
     do {
-        if (passLength > 8 && passLength < 128) {
+        if (passLength >= 8 && passLength <= 128) {
             //determine if length is good
 
             isBad = false;
@@ -30,6 +32,7 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz",
     }
     while (isBad);
     
+    // asking the user which character types desired & validating at least one was chosen
     while (!charPicked) {
         upperPicked = confirm("Do you want uppercase letters?");
         
@@ -42,10 +45,47 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz",
         if (upperPicked || lowerPicked || numPicked || specialPicked) {
             charPicked = true;
         }
-
+        else {
+            alert("You must choose at least one type of character.");
+        }
     }
-    
+    alert("call make pass");
+    makePass(upperPicked, lowerPicked, numPicked, specialPicked, passLength);
+    document.getElementById("password").innerHTML = password;
+}
 
+// computer will create a combo of random choices of the chosen character types until length desired is accomplished
+function makePass (upperPicked, lowerPicked, numPicked, specialPicked, passLength) {
+    alert("making pass");
+    alert("password is " + password + "pass length is " + passLength);
 
-  }
+    while (password.length < passLength) {
+        if (upperPicked) {
+                password+= uppercase.charAt(Math.floor(Math.random()*uppercase.length));
+        }
+        else if (lowerPicked) {
+            for (l = 0; l < lowercase.length; l++) {
+                password += lowercase.charAt(Math.floor(Math.random()*lowercase.length));
+            }
+        }
+        else if (numPicked) {
+            for (var n = 0; n < numbers.length; n++) {
+               console.log(Math.floor(Math.random()*numbers.length));
+            }
+        }
+        else if (specialPicked) {
+            for (s = 0; s < specialChar.length; s++) {
+                console.log(specialChar.charAt(Math.floor(Math.random()*specialChar.length)));
+            }
+        }
+        
+    }
+    alert("password is " + password + "pass length is " + passLength);
+    return password;
+}
 
+  //copy to clipboard button enabled
+  
+ // activate copy function
+
+  // computer copies password to clipboard
